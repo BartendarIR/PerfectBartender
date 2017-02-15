93167df4031b5483d3b0efa4944d9f70ac1f254f
Kevin Ali <kevinali50@yahoo.com>
2017-02-14 20:07:14 -0800

    parse for files and creating the mapping
    I also removed the files that i could not parse well
    I still need to remove duplicate files
    this is a bit trouble some since lots of files

--- src/Data.java
+++ src/Data.java
@@ -1,44 +1,20 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 //This is the raw data I will be parsing the each raw data to a Hash map. so we know how often each words has appeared in the document.
 
 public class Data {
-	private static List<String> list;
-	private static String drinkName;
-	private static String body = "";
+	private List<String> list;
+	private String drinkName;
+	private String body = "";
+	private Map<String, Integer> map;
 
 	public Data(String filename) throws FileNotFoundException {
-		list = Data.init(filename);
-		splitName();
-		//System.out.println(list.size());
-		// Removing the stop words
-		removeStopWord(list);
-		// cleaning up specific words like 's and quotes
-		remove( "'");
-		remove( "”");
-		remove( "“");
-		//System.out.println(list.size());
-
-		// prints the list
-		printList();
-	}
-
-	// Parses through the drink name
-
-	private void splitName() {
-		String[] temp = new String[2];
-		temp = drinkName.split(":");
-		drinkName = temp[1];
-		drinkName = drinkName.trim();
-	}
-	// Initial import of the file to make get the text into the list
-
-	@SuppressWarnings("resource")
-	public static List<String> init(String filename) throws FileNotFoundException {
 		Scanner input;
 		List<String> list = new ArrayList<String>();
 		String line = null;
@@ -50,15 +26,68 @@ public class Data {
 			if (drinkName == null) {
 				drinkName = line;
 			}
-			StringTokenizer strings = new StringTokenizer(line, "[ $*+=-\\?!.,:;()\\/]#&0123456789");
+			StringTokenizer strings = new StringTokenizer(line, "[ –%$*+=-\\?!.,:;()\\/]#&0123456789:");
 			while (strings.hasMoreElements()) {
-				//user is not using realuger single quote i am replacing here also before removing stop words.
+				// user is not using realuger single quote i am replacing here
+				// also before removing stop words.
 				list.add(strings.nextToken().toLowerCase().replace("’", "'"));
 			}
 		}
-		return list;
+		input.close();
+		
+		splitName();
+		// System.out.println(list.size());
+		// Removing the stop words
+		removeStopWord(list);
+		// cleaning up specific words like 's and quotes
+		remove("'",list);
+		remove("”",list);
+		remove("“",list);
+		// System.out.println(list.size());
 
+		// prints the list
+		//printList(list);
+
+		map=map(list);
+		
+		//printMap(map);
+		System.out.println(drinkName);
 	}
+
+	private void printMap(Map<String, Integer> map) {
+		for ( Map.Entry<String , Integer>entry : map.entrySet()) {
+			
+		    System.out.println(entry.getKey() + ": " + entry.getValue());
+		}		
+	}
+
+	// Parses through the drink name
+	private Map<String, Integer> map(List<String> list) {
+		map = new HashMap<String, Integer>();
+		for (String s : list) {
+			if (s.equals(""))
+			{
+				continue;
+			}
+			else if (!map.containsKey(s)) { // first time we've seen this string
+				map.put(s, 1);
+			} else {
+				int count = map.get(s);
+				map.put(s, count + 1);
+			}
+		}
+		return map;
+	}
+
+	private void splitName() {
+		String[] temp = new String[2];
+		temp = drinkName.split(":");
+		drinkName = temp[1];
+		drinkName = drinkName.trim();
+	}
+	// Initial import of the file to make get the text into the list
+
+
 
 	@SuppressWarnings("resource")
 	private static void removeStopWord(List<String> list) throws FileNotFoundException {
@@ -77,13 +106,13 @@ public class Data {
 		}
 	}
 
-	private static void printList() {
+	private  void printList(List<String> list) {
 		for (int x = 0; x < list.size(); x++) {
 			System.out.println(list.get(x));
 		}
 	}
 
-	private static void remove( String s) {
+	private  void remove(String s, List<String> list) {
 		String temp;
 		for (int x = 0; x < list.size(); x++) {
 			if (((String) list.get(x)).contains(s)) {
--- src/main.java
+++ src/main.java
@@ -1,9 +1,24 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 
 public class main {
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
 
-		Data data =new Data("Document/test.txt");
+		File folder = new File("Document/");
+		File[] listOfFiles = folder.listFiles();
+		String s;
+		for (File file : listOfFiles) {
+			if (file.isFile()) {
+				s=file.getName();
+				if(!s.equals("stopword.txt")){
+					
+
+					Data data = new Data("Document/"+s);
+				}
+			}
+		}
+
+		
 	}
 }
